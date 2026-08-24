fd92761e2cdf9bd9af7b75abb98766395c332fab
Muhamad Prinaldi <muhamadprinaldi123@gmail.com>
2026-08-24 18:05:57 +0700

    chore: remove scratch files for 100% clean linting

--- scratch/inspect_karta_slots.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = 'C:/Users/AldiPrinaldi/.gemini/antigravity-ide/brain/d52d4be8-0d9f-4bdb-a64e-21c473c9bd83/.user_uploaded/media_1787306618272.png';
-
-// We can scale the image to 1200 x 3600 px or check relative coordinates
-const scaleX = 1200 / 341;
-const scaleY = 3600 / 1024;
-
-console.log('Scale factor:', scaleX, scaleY);
